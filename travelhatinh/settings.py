398d860e97f4a5c972d047d01f76cc3276bd3caf
Minhle245 <lesyminh21524@gmail.com>
2025-07-14 19:13:34 +0700

    Set DEBUG=False for production

--- travelhatinh/settings.py
+++ travelhatinh/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^zqm^3%%(vnogmta#5u&)8h(7t-7tli&o+8v@d3c+6kx0*y(g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # Cân nhắc đặt False khi triển khai lên production để bảo mật hơn
+DEBUG = False # Cân nhắc đặt False khi triển khai lên production để bảo mật hơn
 
 ALLOWED_HOSTS = ['hatinhgotravel.onrender.com', '127.0.0.1', 'localhost']
 
